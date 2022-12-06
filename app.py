d39e15db188586f43f6786ee913ffd6965691b7a
nMaax <carli.massimiliano01@gmail.com>
2022-12-06 16:02:30 +0100

    Defined a test.html class and his relative Flask decorator

--- app.py
+++ app.py
@@ -9,3 +9,22 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/test')
+def test():
+    
+    # Lista di dizionari
+    pilots = [  {'name': 'Shinji', 'surname': 'Ikari'},
+                {'name': 'Rei', 'surname': 'Ayanami'},
+                {'name': 'Asuka', 'surname': 'Langley Soryu'},
+                {'name': 'Toji', 'surname': 'Suzuhara'},
+                {'name': 'Kaoru', 'surname': 'Nagisa'}
+            ]
+    
+    avaible_pilots = pilots.copy()
+    for avaible_pilot in avaible_pilots:
+        if (avaible_pilot.get('name') == 'Shinji' or avaible_pilot.get('surname') == 'Ikari'):
+            avaible_pilots.remove(avaible_pilot)
+
+    human_pilots=[{'name': 'Shinji', 'surname': 'Ikari'},{'name': 'Asuka', 'surname': 'Langley Soryu'},{'name': 'Toji', 'surname': 'Suzuhara'}]
+    return render_template('test.html', pilots = pilots, avaible_pilots=avaible_pilots, human_pilots=human_pilots)
