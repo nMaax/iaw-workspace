1d6a2fee521b1a5adcc9333a232b280637e896e4
nMaax <carli.massimiliano01@gmail.com>
2022-12-07 17:20:37 +0100

    Defined post page

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from flask import Flask, url_for, render_template
 lorem3 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, molestiae. Impedit rerum vero aperiam qui accusamus? Culpa deserunt veniam voluptatum aliquam, pariatur perferendis quae aperiam, repellat provident quasi vitae maiores? Eius suscipit repellat repudiandae modi sapiente quam eum facilis? Amet, iste quo. Aliquid adipisci optio accusantium voluptates recusandae consequatur ab placeat deserunt voluptatum soluta in hic voluptatem consequuntur, dolores molestiae. Reprehenderit accusamus numquam voluptatibus maiores doloribus, laudantium saepe. Beatae fuga quisquam deserunt vitae odit dolorem nihil facilis ratione velit rerum eaque sit fugit mollitia eos quas maxime magni, esse accusantium!' 
 lorem6 = lorem3*2
 
+articles = [
+        {'id': 1, 'username': 'ikarisan', 'daysago': 2, 'text': lorem6, 'propic':'Shinji_Ikari.jpeg', 'img':'post1.jpg'},
+        {'id': 2, 'username': 'soryuka', 'daysago': 3, 'text': lorem3, 'propic':'Asuka_Soryu.jpeg', 'img':'post2.jpg'},
+        {'id': 3, 'username': 'kaoruni', 'daysago': 5, 'text': lorem3, 'propic':'Kaoru_Nagisa.jpeg', 'img':'post3.jpg'},
+    ]
+
+developers = [
+    {'fullname': 'Ritsuko Akagi', 'description': lorem6, 'motto': 'Nessuno conosce il segreto degli eva...', 'monthsago_update': 3, 'propic':'Ritsuko_Akagi.jpeg'},
+    {'fullname': 'Misato Katsuragi', 'description': lorem3, 'motto': 'Cosa mi nasconde la NERV?', 'monthsago_update': 11, 'propic':'Misato_Katsuragi.jpeg'},
+    {'fullname': 'Asuka Soryu Langley', 'description': lorem6, 'motto': 'Sono nata per pilotare gli eva', 'monthsago_update': 2, 'propic':'Asuka_Soryu.jpeg'}
+]
+    
+
 # Defining app
 app = Flask(__name__)
 
@@ -11,29 +24,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-    articles = [
-        {'username': 'ikarisan', 'daysago': 2, 'text': lorem6, 'propic':'Shinji_Ikari.jpeg', 'img':'post1.jpg'},
-        {'username': 'soryuka', 'daysago': 3, 'text': lorem6, 'propic':'Asuka_Soryu.jpeg', 'img':'post2.jpg'},
-        {'username': 'kaoruni', 'daysago': 5, 'text': lorem6, 'propic':'Kaoru_Nagisa.jpeg', 'img':'post3.jpg'},
-    ]
-    
     return render_template('index.html', articles=articles)
 
 @app.route('/about')
 def about():
-
-    developers = [
-        {'fullname': 'Ritsuko Akagi', 'description': lorem6, 'motto': 'Nessuno conosce il segreto degli eva...', 'monthsago_update': 3, 'propic':'Ritsuko_Akagi.jpeg'},
-        {'fullname': 'Misato Katsuragi', 'description': lorem3, 'motto': 'Cosa mi nasconde la NERV?', 'monthsago_update': 11, 'propic':'Misato_Katsuragi.jpeg'},
-        {'fullname': 'Asuka Soryu Langley', 'description': lorem6, 'motto': 'Sono nata per pilotare gli eva', 'monthsago_update': 2, 'propic':'Asuka_Soryu.jpeg'}
-    ]
-
     return render_template('about.html', developers=developers)
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<int:id>')
+def post(id):
+    return render_template('post.html', article=articles[id-1])
 
 @app.route('/contacts')
 def contacts():
