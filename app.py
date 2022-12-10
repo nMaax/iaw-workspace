0e124cc6e47820a88099a6eac4fde17bbe453b2f
nMaax <carli.massimiliano01@gmail.com>
2022-12-10 17:35:06 +0100

    Completed login management with flask raw session

--- app.py
+++ app.py
@@ -6,17 +6,24 @@ from flask_session import Session
 lorem3 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, molestiae. Impedit rerum vero aperiam qui accusamus? Culpa deserunt veniam voluptatum aliquam, pariatur perferendis quae aperiam, repellat provident quasi vitae maiores? Eius suscipit repellat repudiandae modi sapiente quam eum facilis? Amet, iste quo. Aliquid adipisci optio accusantium voluptates recusandae consequatur ab placeat deserunt voluptatum soluta in hic voluptatem consequuntur, dolores molestiae. Reprehenderit accusamus numquam voluptatibus maiores doloribus, laudantium saepe. Beatae fuga quisquam deserunt vitae odit dolorem nihil facilis ratione velit rerum eaque sit fugit mollitia eos quas maxime magni, esse accusantium!' 
 lorem6 = lorem3*2
 
-articles = [
-        {'id': 1, 'username': 'ikarisan', 'daysago': 2, 'text': lorem6, 'propic':'Shinji_Ikari.jpeg', 'img':'post1.jpg'},
-        {'id': 2, 'username': 'soryuka', 'daysago': 3, 'text': lorem3, 'propic':'Asuka_Soryu.jpeg', 'img':'post2.jpg'},
-        {'id': 3, 'username': 'kaoruni', 'daysago': 5, 'text': lorem3, 'propic':'Kaoru_Nagisa.jpeg', 'img':'post3.jpg'},
-    ]
-
-developers = [
-    {'fullname': 'Ritsuko Akagi', 'description': lorem6, 'motto': 'Nessuno conosce il segreto degli eva...', 'monthsago_update': 3, 'propic':'Ritsuko_Akagi.jpeg'},
-    {'fullname': 'Misato Katsuragi', 'description': lorem3, 'motto': 'Cosa mi nasconde la NERV?', 'monthsago_update': 11, 'propic':'Misato_Katsuragi.jpeg'},
-    {'fullname': 'Asuka Soryu Langley', 'description': lorem6, 'motto': 'Sono nata per pilotare gli eva', 'monthsago_update': 2, 'propic':'Asuka_Soryu.jpeg'}
+admins = [
+    {'username':'akai', 'name': 'Ritsuko', 'surname': 'Akagi', 'description': lorem6, 'motto': 'Nessuno conosce il segreto degli eva...', 'monthsago_update': 3, 'propic':'Ritsuko_Akagi.jpeg'},
+    {'username':'penpen', 'name': 'Misato', 'surname': 'Katsuragi', 'description': lorem3, 'motto': 'Cosa mi nasconde la NERV?', 'monthsago_update': 11, 'propic':'Misato_Katsuragi.jpeg'},
+    {'username':'soryuka', 'name': 'Asuka',  'surname':'Soryu Langley', 'description': lorem6, 'motto': 'Sono nata per pilotare gli eva', 'monthsago_update': 2, 'propic':'Asuka_Soryu.jpeg'}
 ]
+
+users = [
+    {'username':'ikarisan', 'name': 'Shinji', 'surname': 'Ikari', 'propic':'Shinji_Ikari.jpeg'},
+    {'username':'kaoruni', 'name': 'Kaoru', 'surname': 'Nagisa', 'propic':'Kaoru_Nagisa.jpeg'},
+]
+
+users = users + admins
+
+articles = [
+        {'id': 1, 'user': users[0], 'daysago': 2, 'text': lorem6, 'img':'post1.jpg'},
+        {'id': 2, 'user': users[1], 'daysago': 3, 'text': lorem3, 'img':'post2.jpg'},
+        {'id': 3, 'user': users[3], 'daysago': 5, 'text': lorem3, 'img':'post3.jpg'},
+    ]
     
 # Defining app
 app = Flask(__name__)
@@ -27,28 +34,29 @@ bootstrap = Bootstrap5(app)
 
 @app.route('/')
 def index():
-    if not session.get('admin_name'):
-        session['admin_name'] = 'Accedi'
-    return render_template('index.html', articles=articles)
+    #if not session.get('logged_username'):
+    #    session['logged_username'] = 'Accedi'
+    return render_template('index.html', articles=articles, users=users)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', developers=developers)
+    return render_template('about.html', admins=admins, users=users)
 
 @app.route('/post/<int:id>')
 def post(id):
-    return render_template('post.html', article=articles[id-1])
-
-@app.route('/admin_access', methods = ['POST'])
-def admin_access():
-    admin_data = request.form.to_dict()
-    session['admin'] = True
-    session['admin_name'] = admin_data.get('admin_name') 
-    return redirect(url_for('index'))
+    index = id-1
+    return render_template('post.html', article=articles[index], users=users)
 
 @app.route('/contacts')
 def contacts():
-    return render_template('contacts.html')
+    return render_template('contacts.html', users=users)
+
+@app.route('/login', methods = ['POST'])
+def login():
+    #if request.method == "POST" :
+    admin_data = request.form.to_dict()
+    session['logged_username'] = admin_data.get('logged_username') 
+    return redirect(url_for('index'))
 
 # OTHER ROUTES
 
